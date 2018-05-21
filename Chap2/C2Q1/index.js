f13bc8c98c221e8182f8e27a4924eedaa0fcb980
Kryst <Krystropolis@users.noreply.github.com>
2018-05-21 14:43:18 -0700

    fix error in logic. line 24 should add the next node's data, not current. add comments

--- Chap2/C2Q1/index.js
+++ Chap2/C2Q1/index.js
@@ -13,15 +13,22 @@ function Node(data) {
  * @return {object} linked list without duplicates
  */
 const removeDups = l => {
+	// use an object to keep track of all list values
 	let listVals = {};
+	// start with list head node
 	let node = l.head;
+	// add value to list object
 	listVals[node.data] = 1;
 
+	// while the next node exists
 	while (node.next) {
+		// if the next node's value exists in our list value object
 		if (listVals[node.next.data]) {
+			// skip the node
 			node.next = node.next.next;
 		} else {
-			listVals[node.data] = 1;
+			// add the next node's value
+			listVals[node.next.data] = 1;
 			node = node.next;
 		}
 	}
@@ -35,12 +42,20 @@ const removeDups = l => {
 const printList = l => {
 	let node = l.head;
 	let str = '';
+
+	// for each node within the list
 	while (node) {
+		// add the data to the string
 		str += node.data;
+		// advance to next node
 		node = node.next;
+		// if next node exists
 		if (node) {
+			// add arrow
 			str += '->';
+			// if the next node does not exist
 		} else {
+			// print the string
 			return str;
 		}
 	}
